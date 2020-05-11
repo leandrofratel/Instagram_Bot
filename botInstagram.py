dd90872cf33edcb8f7cad363c5cf30b4b5a15bc8
Leandro Fratel de Souza <leandrofratel@gmil.com>
2020-05-11 01:17:04 -0300

    Novos métodos adicionados.

--- botInstagram.py
+++ botInstagram.py
@@ -2,17 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-class InstagramBot:
-    def __init(self, username, password):
+class InstagramBot():
+    def __init__(self, username, password):
+        """Inicialização dos atributos."""
         self.username = username
         self.password = password
-        self.driver = webdriver.Chrome(executable_path=r'chromedriver.exe')
+        self.driver = webdriver.Firefox(executable_path=r'geckodriver.exe')
 
     def login(self):
+        """Inicia o Firefox."""
         driver = self.driver
         driver.get('https://www.instagram.com')
-        #//input[@name='username']
-        #//input[@name='passwrd']
+        sleep(3)
 
-instancia = InstagramBot('heraklless', 'Le@nd42453390')
+        # Identifica o user_name, limpa o campo e preenche com o nome de usuário.
+        user_element = driver.find_element_by_xpath("//input[@name='username']")
+        user_element.clear()
+        sleep(1)
+        user_element.send_keys(self.username)
+
+        # Identifica o campo senha, limpa e preenche com a senha.
+        pass_element = driver.find_element_by_xpath("//input[@name='password']")
+        pass_element.clear()
+        sleep(1)
+        pass_element.send_keys(self.password)
+        pass_element.send_keys(Keys.RETURN)
+        sleep(5)
+
+        # Encontrar o perfil e curtir a primeira foto.
+        self.localizar_e_cutir('gustavoguanabara')
+
+    def localizar_e_cutir(self, perfil):
+        driver = self.driver
+        driver.get('https://www.instagram.com/' + perfil + '/')
+        sleep(1)
+
+        # Clica na primeira foto.
+        pic = driver.find_element_by_class_name("_9AhH0")
+        pic.click()
+        sleep(2)
+
+        # Curti a primeira foto.
+        like = driver.find_element_by_class_name('//button[@class="wpO6b "]') 
+        sleep(2) 
+        like.click()
+
+instancia = InstagramBot('fratel.l', 'Le@nd42453390')
 instancia.login()
+# gustavoguanabara
+# theaigirl
