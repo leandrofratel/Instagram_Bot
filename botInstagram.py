0f166e5ed3ab32127ed8609e19ef78fd57a676ae
Leandro Fratel de Souza <leandrofratel@gmil.com>
2020-05-10 21:52:21 -0300

    Método login adicionado

--- botInstagram.py
+++ botInstagram.py
@@ -1,1 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+class InstagramBot:
+    def __init(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome(executable_path=r'chromedriver.exe')
+
+    def login(self):
+        driver = self.driver
+        driver.get('https://www.instagram.com')
+        #//input[@name='username']
+        #//input[@name='passwrd']
+
+instancia = InstagramBot('heraklless', 'Le@nd42453390')
+instancia.login()
