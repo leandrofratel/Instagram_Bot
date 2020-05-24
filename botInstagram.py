359b1c7fd36751f349e862ed35c882d91a97ab2f
Leandro Fratel de Souza <leandrofratel@gmail.com>
2020-05-24 19:58:00 -0300

    bot configurado.

--- botInstagram.py
+++ botInstagram.py
@@ -13,29 +13,29 @@ class InstagramBot():
         """Inicia o Firefox."""
         driver = self.driver
         driver.get('https://www.instagram.com')
-        sleep(2)
+        sleep(3)
 
         # Identifica o user_name, limpa o campo e preenche com o nome de usuário.
         user_element = driver.find_element_by_xpath("//input[@name='username']")
         user_element.clear()
-        sleep(1)
+        sleep(2)
         user_element.send_keys(self.username)
 
         # Identifica o campo senha, limpa e preenche com a senha.
         pass_element = driver.find_element_by_xpath("//input[@name='password']")
         pass_element.clear()
-        sleep(1)
+        sleep(2)
         pass_element.send_keys(self.password)
         pass_element.send_keys(Keys.RETURN)
-        sleep(7)
+        sleep(10)
 
         # Encontrar o perfil e curtir a primeira foto.
-        self.localizar_e_cutir('gustavoguanabara')
+        self.localizar_e_cutir('"""Coloque o nome do perfil aqui"""')
 
     def localizar_e_cutir(self, perfil):
         driver = self.driver
         driver.get('https://www.instagram.com/' + perfil + '/')
-        sleep(2)
+        sleep(5)
 
         # Clica na primeira foto.
         pic = driver.find_element_by_class_name("_9AhH0")
@@ -53,14 +53,11 @@ class InstagramBot():
         sleep(3)
 
         while True:
-            b_like = driver.find_element_by_class_name("fr66n").click()
-            prx_foto.click()
+            pic_like = driver.find_element_by_class_name("fr66n").click()
             sleep(7)
+            prx_foto.click()
+            sleep(3)
       
 
-instancia = InstagramBot('fratel.', 'Le@nd42453390')
+instancia = InstagramBot('"""Informe seu usuário"""', '"""Informe sua senha"""')
 instancia.login()
-
-# celia_gabbiani44
-# jas_stayfit
-# gustavoguanabara
