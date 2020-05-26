c7259ccaabe0eaad20cd196025c922e5f7d2d318
Leandro Fratel de Souza <leandrofratel@gmil.com>
2020-05-26 20:31:14 -0300

    Atualizado metodo para reproduzir video.

--- main.py
+++ main.py
@@ -13,51 +13,64 @@ class InstagramBot():
         """Inicia o Firefox."""
         driver = self.driver
         driver.get('https://www.instagram.com')
-        sleep(3)
+        sleep(2)
 
         # Identifica o user_name, limpa o campo e preenche com o nome de usuário.
         user_element = driver.find_element_by_xpath("//input[@name='username']")
         user_element.clear()
-        sleep(2)
+        sleep(1)
         user_element.send_keys(self.username)
 
         # Identifica o campo senha, limpa e preenche com a senha.
         pass_element = driver.find_element_by_xpath("//input[@name='password']")
         pass_element.clear()
-        sleep(2)
+        sleep(1)
         pass_element.send_keys(self.password)
         pass_element.send_keys(Keys.RETURN)
-        sleep(10)
+        sleep(7)
 
         # Encontrar o perfil e curtir a primeira foto.
-        self.localizar_e_cutir('"""Coloque o nome do perfil aqui"""')
+        self.localizar(input('Digite o nome do perfil: '))
 
-    def localizar_e_cutir(self, perfil):
+    def localizar(self, perfil):
         driver = self.driver
         driver.get('https://www.instagram.com/' + perfil + '/')
-        sleep(7)
+        sleep(3)
 
         # Clica na primeira foto.
         pic = driver.find_element_by_class_name("_9AhH0")
         pic.click()
-        sleep(5)
-        
-        # Curtir
+        sleep(7)
+
+        # Localiza o botão de like.
         like = driver.find_element_by_class_name("fr66n")
         like.click()
-        sleep(5)
+        sleep(7)
 
-        # Proxima foto.
+        # passa para a proxima foto.
         prx_foto = driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a')
         prx_foto.click()
-        sleep(5)
+        sleep(10)
 
         while True:
-            pic_like = driver.find_element_by_class_name("fr66n").click()
-            sleep(7)
-            prx_foto.click()
-            sleep(7)
-      
+            # Localiza o botão de like.
+            like = driver.find_element_by_class_name("fr66n")
+            like.click()
+            sleep(5)
 
-instancia = InstagramBot('"""Informe seu usuário"""', '"""Informe sua senha"""')
+            # Segundo click na foto, caso seja um video, ele vai reproduzir.
+            try:
+                rep_video = driver.find_element_by_class_name('fXIG0')
+                rep_video.click()
+                sleep(20)
+            except:
+                # passa para a proxima foto.
+                prx_foto.click()
+                sleep(10)
+            else:
+                # passa para a proxima foto.
+                prx_foto.click()
+                sleep(7)
+
+instancia = InstagramBot(input('Insira o seu usuario: '), 'Agora a sua senha: ')
 instancia.login()
